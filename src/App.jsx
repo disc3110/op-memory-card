4de6720dd6812c6bac3fd9a87fbb52bfec9efa58
Diego Solis <disc3110@gmail.com>
2025-02-08 10:29:09 -0800

    add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import { useState } from 'react'
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className="text-center"> 
+      <h1 className="text-3xl font-bold underline">
+        Hello, Tailwind CSS!
+      </h1>
+    </div>
   )
 }
 
