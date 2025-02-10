d8663f97e0215ae153daa352e9800db49b0a1087
Diego Solis <disc3110@gmail.com>
2025-02-09 19:53:13 -0800

    implement basic logic of the game

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,19 @@ import { useState } from 'react'
 
 function App() {
 
+  let cards = {1:'image 1', 2:'image 2', 3:'image 3',4:'image 4', 5:'image 5', 6:'image 6',7:'image 7', 8:'image 8', 9:'image 9',10:'image 10', 11:'image 11', 12:'image 12',}
+
+  let clickedCard = new Set();
+
+  let handleCardClick = (key) => {
+    if (clickedCard.has(key) ){
+      console.log(`you lost`)
+    } else {
+      clickedCard.add(key)
+      console.log(`you clicked card # ${key}`)
+    }
+  }
+
   return (
     <div className="h-screen w-screen bg-gray-600"> 
       <div className='p-4'>
@@ -14,11 +27,15 @@ function App() {
         </div>
       </div>
       <div className="grid grid-cols-3 sm:grid-cols-4 h-[80vh]">
-        {Array.from({ length: 12 }, (_, i) => (
-          <div key={i} className="border-2 border-black outline-4 bg-pink-500 m-4">
-            "Content for div {i + 1} */"
-          </div>
-        ))}
+       {Object.entries(cards).map(([key, card]) => (
+         <div 
+          key={key} 
+          className="border-2 border-black outline-4 bg-pink-500 m-4 hover:bg-sky-700" 
+          onClick={()=> handleCardClick(key)}
+         >
+            {`Content for div ${card}`}
+         </div>
+       ))}
       </div>
     </div>
   )
