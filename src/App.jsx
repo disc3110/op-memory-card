1a3f37403accca76f04a7e59db5cd61bbcd4ea5e
Diego Solis <disc3110@gmail.com>
2025-02-11 14:48:11 -0800

    Add current score

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ function App() {
   let cards = {1:'image 1', 2:'image 2', 3:'image 3',4:'image 4', 5:'image 5', 6:'image 6',7:'image 7', 8:'image 8', 9:'image 9',10:'image 10', 11:'image 11', 12:'image 12',}
 
   let clickedCard = new Set();
+  
+
+  const [clickedCards, setClickedCards] = useState([])
 
   const [imageURLs, setImageURLs] = useState([]);
 
@@ -15,12 +18,13 @@ function App() {
     setImageURLs(newImageURLs);
   },[]);
 
-
+  
   let handleCardClick = (key) => {
-    if (clickedCard.has(key) ){
+    if (clickedCards.includes(key) ){
+      setClickedCards([])
       console.log(`you lost`)
     } else {
-      clickedCard.add(key)
+      setClickedCards(prevSet => [...prevSet, key]);
       console.log(`you clicked card # ${key}`)
     }
   }
@@ -34,6 +38,9 @@ function App() {
           Each time you select an image, all images will shuffle to new positions. The goal is to remember 
           which images you’ve already selected and avoid selecting them again. The game continues until all images 
           have been selected once. Challenge your memory and see how accurately you can recall your previous choices!
+        </div>
+        <div className='text-white'>
+          {`Score: ${clickedCards.length}/12`}
         </div>
       </div>
       <div className="grid grid-cols-3 sm:grid-cols-4 h-[80vh]">
